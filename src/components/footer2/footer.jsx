047f7ba2df4964dffd55ecf21a9cd57d62cd798f
Taha <taha.akber007@gmail.com>
2024-06-24 16:18:03 +0500

    Update footer.jsx

--- src/components/footer2/footer.jsx
+++ src/components/footer2/footer.jsx
@@ -169,7 +169,7 @@ const Footer = () => {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
-                    height="24"
+                    height="25"
                     fill="none"
                   >
                     <path
@@ -183,7 +183,7 @@ const Footer = () => {
                       d="M3.62 8.642c1.97-8.66 14.8-8.65 16.76.01 1.15 5.08-2.01 9.38-4.78 12.04a5.194 5.194 0 0 1-7.21 0c-2.76-2.66-5.92-6.97-4.77-12.05Z"
                     />
                   </svg>
-                  <p>
+                  <p className="p-0.5">
                     Address: 4th floor, Elegant tower, plot no BC-2,
                     <br /> Khayaban-e- Iqbal, Block no 5, KDA <br />
                     Scheme No 5, Clifton, Karachi, Pakistan
