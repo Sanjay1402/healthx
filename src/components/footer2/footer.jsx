c772573a57b29d00b79ac868009346e09b535975
Taha <taha.akber007@gmail.com>
2024-06-24 14:51:54 +0500

    Update footer.jsx

--- src/components/footer2/footer.jsx
+++ src/components/footer2/footer.jsx
@@ -117,34 +117,80 @@ const Footer = () => {
           </div>
           <div>
             Our Information
-            <div className="text-xs mt-3 text-slate-400 ">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="25"
-                  fill="none"
-                >
-                  <path
-                    stroke="#E9501D"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-miterlimit="10"
-                    stroke-width="1.5"
-                    d="M17 20.652H7c-3 0-5-1.5-5-5v-7c0-3.5 2-5 5-5h10c3 0 5 1.5 5 5v7c0 3.5-2 5-5 5Z"
-                  />
-                  <path
-                    stroke="#E9501D"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-miterlimit="10"
-                    stroke-width="1.5"
-                    d="m17 9.152-3.13 2.5c-1.03.82-2.72.82-3.75 0L7 9.152"
-                  />
-                </svg>
-                <p>asdasdasdas</p>
-              </div>
-            </div>
+            <ul>
+              <li>
+                <div className="flex text-xs mt-3 text-slate-400 ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                  >
+                    <path
+                      stroke="#E9501D"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-miterlimit="10"
+                      stroke-width="1.5"
+                      d="M17 20.652H7c-3 0-5-1.5-5-5v-7c0-3.5 2-5 5-5h10c3 0 5 1.5 5 5v7c0 3.5-2 5-5 5Z"
+                    />
+                    <path
+                      stroke="#E9501D"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-miterlimit="10"
+                      stroke-width="1.5"
+                      d="m17 9.152-3.13 2.5c-1.03.82-2.72.82-3.75 0L7 9.152"
+                    />
+                  </svg>
+                  <p className="p-1">info@healthxpakistan.com</p>
+                </div>
+              </li>
+              <li>
+                <div className="flex text-xs mt-3 text-slate-400 ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                  >
+                    <path
+                      stroke="#fff"
+                      stroke-miterlimit="10"
+                      stroke-width="1.5"
+                      d="M21.97 18.482c0 .36-.08.73-.25 1.09-.17.36-.39.7-.68 1.02-.49.54-1.03.93-1.64 1.18-.6.25-1.25.38-1.95.38-1.02 0-2.11-.24-3.26-.73s-2.3-1.15-3.44-1.98a28.75 28.75 0 0 1-3.28-2.8 28.414 28.414 0 0 1-2.79-3.27c-.82-1.14-1.48-2.28-1.96-3.41-.48-1.14-.72-2.23-.72-3.27 0-.68.12-1.33.36-1.93.24-.61.62-1.17 1.15-1.67.64-.63 1.34-.94 2.08-.94.28 0 .56.06.81.18.26.12.49.3.67.56l2.32 3.27c.18.25.31.48.4.7.09.21.14.42.14.61 0 .24-.07.48-.21.71-.13.23-.32.47-.56.71l-.76.79c-.11.11-.16.24-.16.4 0 .08.01.15.03.23.03.08.06.14.08.2.18.33.49.76.93 1.28.45.52.93 1.05 1.45 1.58.54.53 1.06 1.02 1.59 1.47.52.44.95.74 1.29.92.05.02.11.05.18.08.08.03.16.04.25.04.17 0 .3-.06.41-.17l.76-.75c.25-.25.49-.44.72-.56.23-.14.46-.21.71-.21.19 0 .39.04.61.13.22.09.45.22.7.39l3.31 2.35c.26.18.44.39.55.64.1.25.16.5.16.78Z"
+                    />
+                  </svg>
+                  <p className="p-1">021 382 824 33</p>
+                </div>
+              </li>
+              <li>
+                <div className="flex text-xs mt-3 text-slate-400 ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="25"
+                    fill="none"
+                  >
+                    <path
+                      stroke="#fff"
+                      stroke-width="1.5"
+                      d="M12 13.582a3.12 3.12 0 1 0 0-6.24 3.12 3.12 0 0 0 0 6.24Z"
+                    />
+                    <path
+                      stroke="#fff"
+                      stroke-width="1.5"
+                      d="M3.62 8.642c1.97-8.66 14.8-8.65 16.76.01 1.15 5.08-2.01 9.38-4.78 12.04a5.194 5.194 0 0 1-7.21 0c-2.76-2.66-5.92-6.97-4.77-12.05Z"
+                    />
+                  </svg>
+                  <p>
+                    Address: 4th floor, Elegant tower, plot no BC-2,
+                    <br /> Khayaban-e- Iqbal, Block no 5, KDA <br />
+                    Scheme No 5, Clifton, Karachi, Pakistan
+                  </p>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
