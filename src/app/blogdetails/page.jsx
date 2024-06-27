dfd72f4ed8ea28ea16d7fee02ef3eb1c51fe502f
Taha <taha.akber007@gmail.com>
2024-06-27 16:12:33 +0500

    complete blogdetails page and also added responsiveness to page and footer

--- src/app/blogdetails/page.jsx
+++ src/app/blogdetails/page.jsx
@@ -11,7 +11,7 @@ const Page = ({}) => {
       <div className="mb-5 pb-5 m-1">
         <CurrentPage pages_text={pages_text} />
       </div>
-      <div className="flex">
+      <div className="flex flex-row flex-wrap md:flex-nowrap lg:flex-nowrap">
         <div className="flex flex-col w-30 flex-wrap">
           <div className="m-1">
             <p className="text-xl font-light">Jan 11.24</p>
@@ -27,13 +27,14 @@ const Page = ({}) => {
               alt=""
               width={800}
               height={100}
+              className="max-w-full h-auto"
             />
           </div>
         </div>
-        <div className="flex flex-col gap-y-32 mr-2 pr-2 flex-wrap">
-          <div className="max-w-sm bg-white border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700 p-5 m-2 flex flex-wrap">
+        <div className="flex flex-col gap-y-7 md:gap-y-24 md:mr-2 md:pr-2  md:flex-wrap lg:flex-wrap">
+          <div className="max-w-sm bg-white border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700 md:p-5 md:m-2 lg:p-5 lg:m-2 flex flex-wrap">
             <p className="green pr-5 m-2 text-xl font-medium">On this Page</p>
-            <div className="flex flex-wrap items-center justify-center">
+            <div className="flex flex-wrap items-center justify-center text-sm md:text-md lg:text-lg">
               <ul className="list-disc list-inside text-wrap">
                 <li className="m-1">Factors Behind Rising Healthcare Cost</li>
                 <li className="m-1">Medical Advancements</li>
@@ -44,10 +45,10 @@ const Page = ({}) => {
               </ul>
             </div>
           </div>
-          <div className="max-w-sm  bg-white border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700 p-4 flex flex-wrap flex-col">
+          <div className="max-w-sm  bg-white border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700 p-5 m-2 flex flex-wrap flex-col">
             <div className="m-2">
               <div>Instagram</div>
-              <div className="orange text-xl">Healthx Pakistan</div>
+              <div className={`${styles.healthx}`}>Healthx Pakistan</div>
             </div>
             <div className="flex flex-col gap-4">
               <div className="flex justify-around ">
@@ -108,7 +109,7 @@ const Page = ({}) => {
           </div>
         </div>
       </div>
-      <div className="w-8/12 flex text-wrap my-8">
+      <div className="w-8/12 flex text-wrap  text-sm md:text-md lg:text-lg">
         <p>
           In recent years, the escalating cost of healthcare has become a
           growing concern worldwide, including in Pakistan. As medical
@@ -122,7 +123,7 @@ const Page = ({}) => {
         </p>
       </div>
       <div className="w-8/12 my-8">
-        <h1 className="font-semibold text-lg">
+        <h1 className="font-semibold text-sm md:text-md lg:text-lg">
           Understanding the Factors Behind Rising Healthcare Costs:
         </h1>
         <p className="font-light">
@@ -132,7 +133,7 @@ const Page = ({}) => {
       </div>
       <div className="w-8/12 my-8">
         <h1 className="font-light orange text-lg">Medical Advancements</h1>
-        <p className="font-light">
+        <p className="font-light text-sm md:text-md lg:text-lg">
           While medical innovations have undoubtedly improved patient outcomes
           and quality of care, they often come at a hefty price. Advanced
           diagnostic tools, cutting-edge treatments, and sophisticated medical
@@ -140,8 +141,8 @@ const Page = ({}) => {
         </p>
       </div>
       <div className="w-8/12 my-8">
-        <h1 className="font-light orange text-lg">Aging Population</h1>
-        <p className="font-light">
+        <h1 className="font-light orange text-lg ">Aging Population</h1>
+        <p className="font-light text-sm md:text-md lg:text-lg ">
           With improvements in healthcare leading to longer life expectancy, the
           proportion of elderly individuals requiring medical attention has
           increased. The elderly often require more intensive and specialized
@@ -150,7 +151,7 @@ const Page = ({}) => {
       </div>
       <div className="w-8/12 my-8">
         <h1 className="font-light orange text-lg">Increased Demand </h1>
-        <p className="font-light">
+        <p className="font-light text-sm md:text-md lg:text-lg">
           As awareness about healthcare grows and access to medical facilities
           improves, there is a surge in demand for healthcare services. This
           increased demand exerts pressure on existing healthcare
@@ -158,7 +159,7 @@ const Page = ({}) => {
           the population.
         </p>
       </div>
-      <div className="w-8/12 my-8 flex  justify-between">
+      <div className="w-8/12 my-8 flex  justify-between flex-wrap gap-5">
         <div>
           <Image src="/Rectangle 201.png" alt="" width={370} height={100} />
         </div>
@@ -171,7 +172,7 @@ const Page = ({}) => {
           <h1 className="font-semibold text-lg">
             Is It Too Late to Bring Costs Down?{" "}
           </h1>
-          <p className="font-light">
+          <p className="font-light text-sm md:text-md lg:text-lg">
             While the challenge of controlling healthcare costs may seem
             daunting, it is not insurmountable. Several strategies can be
             implemented to address this issue:
@@ -181,7 +182,7 @@ const Page = ({}) => {
           <h1 className="font-light orange text-lg">
             Embrace Preventive Healthcare:
           </h1>
-          <p className="font-light">
+          <p className="font-light text-sm md:text-md lg:text-lg">
             Investing in preventive measures and promoting healthy lifestyle
             choices can reduce the incidence of chronic diseases and lower
             healthcare costs in the long run. Public health initiatives focusing
@@ -194,36 +195,43 @@ const Page = ({}) => {
           <h1 className="font-light orange text-lg">
             Promote Generic Medications
           </h1>
-          <p className="font-light">
+          <p className="font-light text-sm md:text-md lg:text-lg">
             Encouraging the use of generic medications over brand-name drugs can
             significantly reduce medication costs for patients. Government
             policies that promote generic drug manufacturing and increase access
             to affordable alternatives can help mitigate the financial burden on
             individuals.
           </p>
+          wrap
         </div>
       </div>
-      <div className="flex flex-col flex-wrap justify-start">
-        <div className="text-xl my-4 py-5">
+      <div className="flex flex-col text-wrap justify-start">
+        <div className="text-xl my-4 py-4">
           <h1 className="font-bold text-3xl green">Post a Comment</h1>
           <p className="font-light">Understanding the Impact and Solutions</p>
         </div>
 
-        <div className="mt-10 mb-10 text-center flex flex-col flex-wrap">
-          <div className="grid grid-cols-2 gap-5 ">
-            <Input
-              className="mb-5 orange-border"
-              type="text"
-              placeholder="Name"
-            />
-            <Input
-              className="mb-5 orange-border"
-              type="email"
-              placeholder="Email"
-            />
+        <div className="mt-10 mb-10 text-center flex flex-col flex-wrap gap-1">
+          <div className="flex flex-row justify-between flex-wrap gap-1">
+            <div className="max-w-xl w-full">
+              <Input
+                className="orange-border h-12 px-3"
+                type="text"
+                placeholder="Name"
+              />
+            </div>
+            <div className="max-w-xl w-full">
+              <Input
+                className="orange-border h-12 px-3"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
           </div>
-          <Textarea className="mb-5 orange-border" placeholder="Message" />
-          <Button className="bg_orange w-1/6">Send a message</Button>
+          <Textarea className="mb-3 orange-border " placeholder="Message" />{" "}
+          <Button className="bg_orange w-1/6 text-wrap text-sm">
+            Send a message
+          </Button>
         </div>
       </div>
       <div className="flex mt-10 mb-5 justify-start flex-wrap">
@@ -239,11 +247,11 @@ const Page = ({}) => {
         <div className="mb-5 flex justify-between flex-wrap">
           <div className="">
             <p className="text-xs">Jan 11:24</p>
-            <h1 className="text-sm font-medium">
+            <h1 className="text-sm md:text-md lg:text-md font-medium">
               Your Daily Dose of Medical Wisdom
             </h1>
 
-            <div className="max-w-sm my-3">
+            <div className="max-w-sm my-4">
               <a href="#">
                 <img
                   className="rounded-t-lg my-2"
@@ -259,10 +267,10 @@ const Page = ({}) => {
           </div>
           <div>
             <p className="text-xs">Jan 11:24</p>
-            <h1 className="text-sm font-medium">
+            <h1 className="text-sm md:text-md lg:text-md font-medium">
               Empowering Communities through Health Education
             </h1>
-            <div className="max-w-sm my-3">
+            <div className="max-w-sm my-4">
               <a href="#">
                 <img
                   className="rounded-t-lg my-2"
@@ -278,10 +286,10 @@ const Page = ({}) => {
           </div>
           <div>
             <p className="text-xs">Jan 11:24</p>
-            <h1 className="text-sm font-medium">
+            <h1 className="text-sm md:text-md lg:text-md font-medium">
               Empowering Communities through Health Education
             </h1>
-            <div className="max-w-sm my-3">
+            <div className="max-w-sm my-4">
               <a href="#">
                 <img
                   className="rounded-t-lg my-2"
